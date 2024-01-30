8ba86a29c14a3737473cdb57128b5b7ead42dabe
sbemjr1 <sbemjr1@naver.com>
2024-01-30 20:44:22 +0900

    0130 재귀 과제 및 스터디 문제

---
+++ algorithm/sbemjr1/BOJ_N과M1.java
@@ -0,0 +1,42 @@
+package sbemjr1;
+
+import java.util.Scanner;
+
+public class BOJ_N과M1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[] arr = new int[N];
+		int[] sel = new int[M];
+		boolean[] v = new boolean[arr.length];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = i+1;
+		}
+		
+		recursive(arr,sel, v,0);
+	}
+
+	private static void recursive(int[] arr, int[] sel, boolean[] v, int k) {
+		if(k==sel.length) {
+			for (int j = 0; j < sel.length; j++) {
+				System.out.print(sel[j]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(v[i] == false) {
+				v[i] = true;
+				sel[k] = arr[i];
+				recursive(arr,sel,v,k+1);
+				v[i] = false;
+			}
+		}
+	}
+
+}
